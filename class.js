6fe98ea8b9fe4726110312f574b6b350fd9c0e53
Tom Andersen <tomandersen2000@gmail.com>
2016-03-08 22:41:24 -0800

    Ability to edit properties of tiles
    Signed-off-by: Tom Andersen <tomandersen2000@gmail.com>

--- class.js
+++ class.js
@@ -1,10 +1,19 @@
 "use strict";
 
 class Tile {
-	constructor(imgIndex, className, x, y) {
-		this.imgIndex = imgIndex;
-		this.className = className;
+	constructor(createClassOptions, x, y) {
+		this.image = new Image();
+		this.className = "";
+		this.imageURL = "/not_found.png";
 		this.x = x;
 		this.y = y;
+
+		for (var variable in createClassOptions) {
+			if (createClassOptions.hasOwnProperty(variable)) {
+				this[variable] = createClassOptions[variable];
+			}
+		}
+
+		this.image.src = this.imageURL;
 	}
 }
